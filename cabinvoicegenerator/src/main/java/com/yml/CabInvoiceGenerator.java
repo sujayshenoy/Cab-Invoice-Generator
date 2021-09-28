7fc575b140973ae8eff93818fa84cb571d4c5379
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-09-28 18:49:42 +0530

    Initialized Maven project

---
+++ cabinvoicegenerator/src/main/java/com/yml/CabInvoiceGenerator.java
@@ -0,0 +1,13 @@
+package com.yml;
+
+/**
+ * Hello world!
+ *
+ */
+public class CabInvoiceGenerator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Cab Invoice Generator Program" );
+    }
+}
---
+++ cabinvoicegenerator/src/test/java/com/yml/CabInvoiceGeneratorTest.java
@@ -0,0 +1,20 @@
+package com.yml;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CabInvoiceGeneratorTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
