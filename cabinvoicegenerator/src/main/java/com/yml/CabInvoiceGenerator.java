5f67502ce71865f3cfea5cead491a5762fe8ce15
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-09-28 19:18:50 +0530

    Added method to calculate total fare of a journey

--- cabinvoicegenerator/src/main/java/com/yml/CabInvoiceGenerator.java
+++ cabinvoicegenerator/src/main/java/com/yml/CabInvoiceGenerator.java
@@ -6,8 +6,13 @@ package com.yml;
  */
 public class CabInvoiceGenerator 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to Cab Invoice Generator Program" );
+    private final int MIN_FARE = 5;
+
+    public double generateFare(int distance, int time) {
+        double cost = distance * 10;
+        cost += time * 1;
+
+        return cost > MIN_FARE ? cost : MIN_FARE;
     }
+       
 }
